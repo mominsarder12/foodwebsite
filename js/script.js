0401364847cedd5a1983d96126a74c1cd5d8c495
momin <momin.sarder1212@gmail.com>
2022-08-30 00:50:22 +0600

    first commit 12:50am on augest 30 2022

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuBar = document.querySelector(".menu-bar");
+const navLinks = document.querySelector(".nav-links");
+
+
+menuBar.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+
+
+});
